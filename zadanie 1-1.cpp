acb8d9dd6b2154ff256fb659b1a5dd01fd6af918
ToschevKD <160127099+ToschevKD@users.noreply.github.com>
2024-05-23 13:55:04 +0300

    Update zadanie 1-1.cpp

--- zadanie 1-1.cpp
+++ zadanie 1-1.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+/**
+ * @brief Функция getA вычисляет результат по формуле a = x * 2 + y - z.
+ * 
+ * @param x Первый аргумент для вычисления a.
+ * @param y Второй аргумент для вычисления a.
+ * @param z Третий аргумент для вычисления a.
+ * @return Результат вычисления по формуле a = x * 2 + y - z.
+ */
 double getA(const double x, const double y, const double z);
+
+/**
+ * @brief Функция getB вычисляет результат по формуле b = x + y * 3 - z.
+ * 
+ * @param x Первый аргумент для вычисления b.
+ * @param y Второй аргумент для вычисления b.
+ * @param z Третий аргумент для вычисления b.
+ * @return Результат вычисления по формуле b = x + y * 3 - z.
+ */
 double getB(const double x, const double y, const double z);
+
+/**
+ * @brief Главная функция программы.
+ * 
+ * @return 0 в случае успешного завершения программы.
+ */
+int main();
 int main() {
   const double x=-2.9;
   const double y=15.5;
