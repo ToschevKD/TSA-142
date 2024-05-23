7c2db3e81f949f22bf22948929dd082bb9d5cc79
ToschevKD <160127099+ToschevKD@users.noreply.github.com>
2024-05-23 13:53:20 +0300

    Update zadanie 3.cpp

--- zadanie 3.cpp
+++ zadanie 3.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+/**
+ * @brief Функция getA вычисляет результат по формуле a = x * 2 + y - z.
+ * 
+ * @param x Первый аргумент для вычисления a.
+ * @param y Второй аргумент для вычисления a.
+ * @param z Третий аргумент для вычисления a.
+ * @return Результат вычисления по формуле a = x * 2 + y - z.
+ */
+double getA(const double x, const double y, const double z);
+
+/**
+ * @brief Функция getB вычисляет результат по формуле b = x + y * 3 - z.
+ * 
+ * @param x Первый аргумент для вычисления b.
+ * @param y Второй аргумент для вычисления b.
+ * @param z Третий аргумент для вычисления b.
+ * @return Результат вычисления по формуле b = x + y * 3 - z.
+ */
+double getB(const double x, const double y, const double z);
+
+/**
+ * @brief Главная функция программы.
+ * 
+ * @return 0 в случае успешного завершения программы.
+ */
+int main();
 int main() {
-  double t1, t2, t3, v1, v2, v3;
-  double totalDistance;
-
-  cout << "Напишите время пройденное со скоростью v1 (в часах): ";
-  cin >> t1;
-  cout << "Введите скорость v1 (в км/ч): ";
-  cin >> v1;
-
-  cout << "Напишите время пройденное со скоростью v2 (в часах): ";
-  cin >> t2;
-  cout << "Введите скорость v2 (в км/ч): ";
-  cin >> v2;
-  cout << "Напишите время пройденное со скоростью v3 (в часах): ";
-  cin >> t3;
-  cout << "Введите скорость v3 (в км/ч): ";
-  cin >> v3;
-  totalDistance = t1 * v1 + t2 * v2 + t3 * v3;
-  double halfDistance = totalDistance / 2;
-  double timeToHalfway = 0;
-  if (halfDistance <= t1 *v1) {
-    timeToHalfway = halfDistance / v1;
-  } else if (halfDistance <= (t1 * v1) + (t2 * v2)) {
-    timeToHalfway = t1 + (halfDistance - (t1 * v1)) / v2;
-  } else {
-    timeToHalfway = t1 + t2 + (halfDistance - t1 * v1 - t2 * v2) / v3;
-  }
-  cout << "Время до половины пути: " << timeToHalfway << " часов" << endl;
-  return 0;
+  const double x=-2.9;
+  const double y=15.5;
+  const double z=1.5;
+  cout << "x=" << x << "y=" << y << "z=" << z << endl;
+         cout << "a=" << getA(x,y,z) << "b=" << getB(x,y,z) << endl;
+return 0;
+}
+double getA(const double x, const double y, const double z)
+{
+  return sqrt(pow(x,2)+y)-pow(y,2)*pow(sin(x+z/x),3);
+}
+  double getB(const double x, const double y, const double z)
+{
+  return pow(cos(pow(x,3)),2)-x/sqrt(pow(z,2)+pow(y,2));
 }
