2cec769003d50e34ac177fc4d1c35f29980ce33b
ToschevKD <160127099+ToschevKD@users.noreply.github.com>
2024-06-18 12:42:18 +0300

    Update zadanie 3.cpp

--- zadanie 3.cpp
+++ zadanie 3.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
 using namespace std;
 /**
- * Функция для расчета времени, за которое путник одолел первую половину пути
- * @param t1 Время движения первой части пути
- * @param v1 Скорость движения первой части пути
- * @param t2 Время движения второй части пути
- * @param v2 Скорость движения второй части пути
- * @param t3 Время движения третьей части пути
- * @param v3 Скорость движения третьей части пути
- * @return Время, за которое путник одолел первую половину пути
+ * Функция для расчета времени, за которое путник одолеет первую половину пути
+ * и время для планирования привала
+ * 
+ * @param t1 Время движения со скоростью v1
+ * @param v1 Скорость движения во время t1
+ * @param t2 Время движения со скоростью v2
+ * @param v2 Скорость движения во время t2
+ * @param t3 Время движения со скоростью v3
+ * @param v3 Скорость движения во время t3
+ * @return Время для одоления первой половины пути и время для привала
  */
-double calculateFirstHalfTime(double t1, double v1, double t2, double v2, double t3, double v3) {
-    double totalDistance = (t1 * v1) + (t2 * v2) + (t3 * v3);
+pair<double, double> calculateTimes(double t1, double v1, double t2, double v2, double t3, double v3) {
+    // Расчет расстояния, которое нужно пройти путнику
+    double totalDistance = t1*v1 + t2*v2 + t3*v3;
+
+    // Расчет времени для одоления первой половины пути
     double halfDistance = totalDistance / 2.0;
+    double timeForHalf = 0.0;
 
-    double firstHalfTime = 0.0;
-    double distanceCovered = 0.0;
-
-    // Рассчитываем время для движения до достижения половины пути
-    if (halfDistance <= (t1 * v1)) {
-        firstHalfTime = halfDistance / v1;
-    } else if (halfDistance <= ((t1 * v1) + (t2 * v2))) {
-        firstHalfTime = t1 + ((halfDistance - (t1 * v1)) / v2);
+    if (halfDistance <= t1*v1) {
+        timeForHalf = halfDistance / v1;
+    } else if (halfDistance <= t1*v1 + t2*v2) {
+        timeForHalf = t1 + (halfDistance - t1*v1) / v2;
     } else {
-        firstHalfTime = t1 + t2 + ((halfDistance - (t1 * v1) - (t2 * v2)) / v3);
+        timeForHalf = t1 + t2 + (halfDistance - t1*v1 - t2*v2) / v3;
     }
 
-    return firstHalfTime;
+    // Расчет времени для привала
+    double timeForRest = t1 + t2 + t3;
+
+    return make_pair(timeForHalf, timeForRest);
 }
 
 int main() {
     double t1, v1, t2, v2, t3, v3;
 
+    // Ввод времени и скорости для каждого участка пути
     cout << "Введите время t1 и скорость v1 для первой части пути: ";
     cin >> t1 >> v1;
 
@@ -41,8 +47,11 @@ int main() {
     cout << "Введите время t3 и скорость v3 для третьей части пути: ";
     cin >> t3 >> v3;
 
-    double firstHalfTime = calculateFirstHalfTime(t1, v1, t2, v2, t3, v3);
-    cout << "Время, за которое путник одолел первую половину пути: " << firstHalfTime << " часов" << endl;
+    // Вызов функции для расчета времени для первой половины пути и времени для привала
+    auto times = calculateTimes(t1, v1, t2, v2, t3, v3);
+
+    // Вывод результатов
+    cout << "Время, за которое путник одолел первую половину пути: " << times.first << " часов" << endl;
 
     return 0;
 }
